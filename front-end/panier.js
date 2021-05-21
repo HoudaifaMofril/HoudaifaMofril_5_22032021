ebbadf2e6bce8cda799572364f35d7a4240c01e0
Houdaïfa Mofril <houdaifadevelopper@gmail.com>
2021-05-21 21:17:16 +0200

    Mise en place méthode fetch dans la boucle pour récupérer les prix des articles directement depuis l'API, suppression de la fonction calculPrixPanier(), déclaration et initialisation de la variable total et totalPanier pour itérer le prix des articles et afficher le prix total du panier

--- front-end/panier.js
+++ front-end/panier.js
@@ -2,12 +2,31 @@ const inHtml = document.getElementById("main");
 const prixInHtml = document.getElementById("finalPrice");
 const btnCommande = document.getElementById("btnCom");
 let data = JSON.parse(localStorage.getItem("cart"));
+let total= 0; // On déclare et j'initalise une variable total. Cette variable recevra la totalité du prix du panier
 
 if (localStorage.length > 0) {
-    prixInHtml.innerHTML = calculPrixPanier() + " €uros "; //--Fonction qui affiche le prix total de la commande
+
 
     data.forEach((objet) => { //-- On céer une boucle pour parcourir chaque produits ajouter dans le localStorage et l'afficher dynamiquement sur la page
-        inHtml.innerHTML += `
+    
+
+        fetch(`http://localhost:3000/api/teddies/${objet._id}`)// On utilise la méthode fetch dans une boucle pour récupérer les prix de chaque article directement depuis l'API
+
+            .then(response => {
+                if (response.ok) {
+
+                    return datas = response.json()
+
+                } else {
+                    Promise.reject(response.status);
+                };
+
+            })
+            .then(datas => {
+                let totalPrice = (datas.price / 100) * objet.quantite; // On déclare et initalise la variable totalPrice pour avoir le prix total de chaque article
+                total += totalPrice; // On itére la variable total avec le résultat de la variable totalPrice pour faire l'addition du prix total du Panier
+
+                inHtml.innerHTML += `
             <div class="row m-2 pt-3 panierLine">
                 <div class="col-md-3 col-lg-2">
                     <img alt="${objet.name}" class="img-fluid" src="${objet.image}">
@@ -18,14 +37,19 @@ if (localStorage.length > 0) {
                     <p><strong>Couleur</strong> : ${objet.colors}</p>
                 </div>
                 <div class="col-md-5 col-lg-4"
-                    <p class="prixProduitPanier"><strong>Prix : <span>${objet.totalPrice} €</span></strong></p>   
+                    <p class="prixProduitPanier"><strong>Prix : <span>${totalPrice} €</span></strong></p>   
                 </div>
                 <div class="col-md-1">
                     <button class="btn btn-danger mb-3" onclick="deleteItem('${objet._id}', '${objet.colors}')">Supprimer</button>  
                 </div>
             </div>
-            `;
+            `
+                prixInHtml.innerHTML = total + " euros "; // On affiche dynamiquement le prix total du panier sur la page panier
+                
+                ;
+            });
     });
+
 } else {
 
     inHtml.innerHTML = `
@@ -35,13 +59,13 @@ if (localStorage.length > 0) {
             
             
         </div>`
-        prixInHtml.innerHTML = "Aucun articles dans votre panier !";
+    prixInHtml.innerHTML = "Aucun articles dans votre panier !";
 };
 
 //-- Fonction pour supprimer un produit du panier,donc du localStorage, grâce à son _id et à sa couleur
 
 function deleteItem(_id, color) {
- 
+
     const lsUpdate = data.filter((objet) => objet._id !== _id || objet.colors !== color);
     localStorage.setItem("cart", JSON.stringify(lsUpdate));
 
@@ -49,16 +73,6 @@ function deleteItem(_id, color) {
         localStorage.clear();
     }
     document.location.href = "panier.html";
-};
-
-//-- Calcul du prix total Panier
-
-function calculPrixPanier() {
-    let totalPriceItem = data.reduce((accumulator, item) => {
-        return accumulator + item.totalPrice;
-    }, 0);
-
-    return totalPriceItem;
 };
 
 /************* On créer des constantes pour le formulaire ******************/
@@ -85,10 +99,12 @@ form.addEventListener("submit", (e) => {
         email: email.value,
     };
 
-    
 
     const products = []; // Teddies en tant que tableau à envoyer en POST
-    const donnees = { contact, products }; // Créer donnees comme objet contact + tableau products
+    const donnees = {
+        contact,
+        products
+    }; // Créer donnees comme objet contact + tableau products
 
     data.forEach((teddies) => {
         products.push(teddies._id);
@@ -103,10 +119,10 @@ form.addEventListener("submit", (e) => {
             "Content-Type": "application/json",
         },
     };
-//-- On vérifie que l'on attend les bonnes informations dans les champs de contact et que ce ci ne sont pas vides avant envoi au back-end
-    if (firstName.value.length == ""  || lastName.value.length == "" || address.value.length == "" || city.value.length == "" || email.value.length == "" ) {
-        alert("Tous les champs doivent êtres remplis !") 
-        
+    //-- On vérifie que l'on attend les bonnes informations dans les champs de contact et que ce ci ne sont pas vides avant envoi au back-end
+    if (firstName.value.length == "" || lastName.value.length == "" || address.value.length == "" || city.value.length == "" || email.value.length == "") {
+        alert("Tous les champs doivent êtres remplis !")
+
 
     } else {
         // Requête POST
@@ -114,7 +130,7 @@ form.addEventListener("submit", (e) => {
             // On reçoit les données du back
             .then(response => { // On reçoit une première prommesse
                 if (response.ok) {
-                    
+
                     return response.json() // Si la response est OK alors , on nous retourne un objet JSON
                 } else {
                     Promise.reject(response.status); // Sinon , on recoit le statut de l'érreur
@@ -123,9 +139,9 @@ form.addEventListener("submit", (e) => {
 
             // Traitement pour l'obtention du numéro de commmande
             .then((datas) => {
-                
+
                 const orderId = datas.orderId;
-                const priceTotalOrder = calculPrixPanier();
+                const priceTotalOrder = total;
 
                 window.location.href = `confirm.html?order_number=${orderId}&order_price=${priceTotalOrder}`;
 
@@ -134,9 +150,8 @@ form.addEventListener("submit", (e) => {
             .catch((error) => {
                 alert(error);
             });
-            
+
     }
-    
+
 
 });
-
